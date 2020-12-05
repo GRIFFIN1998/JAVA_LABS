6204388b6e2f77fc870a8179b8ff11f00bd6bfaa
GRIFFIN1998 <ceban_yaroslav@mail.ru>
2020-12-05 09:54:04 +0200

    Ex 14,7

---
+++ java_labs/src/GasMileage.java
@@ -0,0 +1,23 @@
+public class GasMileage {
+
+    private double miles, gallons;
+
+    public double getTripMPG(int miles, int gallons) {
+        this.miles += miles;
+        this.gallons += gallons;
+
+        return (double) miles / gallons;
+    }
+
+    public double getMiles() {
+        return miles;
+    }
+
+    public double getGallons() {
+        return gallons;
+    }
+
+    public double getMPG() {
+        return miles / gallons;
+    }
+}
